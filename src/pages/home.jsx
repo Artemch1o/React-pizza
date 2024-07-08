f634cdf9db7f51c394376d0a575f0e0e07126f2c
ArtemVasiliev <6524652@mail.ru>
2024-07-08 10:12:41 +0300

    feat:added functionality for sorting and filtering pizzas

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,9 +9,19 @@ import pizzas from "../assets/piazzas.json";
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "Популярности",
+    StyleProperty: "rating",
+  });
 
   React.useEffect(() => {
-    fetch("https://668a6aee2c68eaf3211cc04f.mockapi.io/items")
+    setIsLoading(true);
+    fetch(
+      `https://668a6aee2c68eaf3211cc04f.mockapi.io/items?${
+        categoryId > 0 ? `category=${categoryId}` : ""
+      }&sortBy=${sortType.sortProperty}&order=desc`, 
+    )
       .then((res) => {
         res.json();
       })
@@ -19,29 +29,23 @@ const Home = () => {
         setItems(arr);
         setIsLoading(false);
       });
-      window.scrollTo(0,0)
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(id) => setCategoryId(id)}
+        />
+        <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : pizzas.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
+          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
